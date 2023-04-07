549d1bfe686a30b6f87e0a6d9cb19a4856e38b79
dolappo <omodolappo@gmail.com>
2023-04-07 23:26:41 +0100

    profile screen

--- lib/ui/screens/main_screen/home/pages/profile.dart
+++ lib/ui/screens/main_screen/home/pages/profile.dart
@@ -5,6 +5,12 @@ class ProfilePage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Column(
+      children: [
+        Column(
+          children: [],
+        )
+      ],
+    );
   }
 }
